d28f04bfc0de45f786593bf570ee2bc6b6b1fc12
Gavin Lobo <gavin.n.lobo@gmail.com>
2019-03-10 19:14:28 -0400

    overwriting old cmake files

--- .ci-files/install_cmake.sh
+++ .ci-files/install_cmake.sh
@@ -11,6 +11,9 @@ VERSION=3.13.4
 
 wget -q https://github.com/Kitware/CMake/releases/download/v${VERSION}/cmake-${VERSION}-Linux-x86_64.tar.gz -O cmake.tar.gz
 tar -xzf cmake.tar.gz
-sudo cp -r cmake-${VERSION}*/* /usr
+
+# Remove the original files and copy the new ones over.
+sudo rm -rf /usr/local/cmake-3.12.4/*
+sudo cp -r cmake-${VERSION}*/* /usr/local/cmake-3.12.4/
 which cmake
 cmake --version
