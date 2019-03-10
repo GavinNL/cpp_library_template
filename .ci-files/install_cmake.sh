cca5c61f8b481ebe59ba2dfa39856a7bfb389453
Gavin Lobo <gavin.n.lobo@gmail.com>
2019-03-10 19:36:55 -0400

    debug

--- .ci-files/install_cmake.sh
+++ .ci-files/install_cmake.sh
@@ -5,15 +5,22 @@ set -x
 
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     echo "Darwin!"
+    brew install cmake
+else
+
+
+    VERSION=3.13.4
+
+    which cmake 
+    wget -q https://github.com/Kitware/CMake/releases/download/v${VERSION}/cmake-${VERSION}-Linux-x86_64.tar.gz -O cmake.tar.gz
+    tar -xzf cmake.tar.gz
+
+    # Remove the original files and copy the new ones over.
+    sudo rm -rf /usr/local/cmake-3.12.4/*
+    sudo cp -r cmake-${VERSION}*/* /usr/local/cmake-3.12.4/
+    which cmake
+
+
 fi
 
-VERSION=3.13.4
-
-wget -q https://github.com/Kitware/CMake/releases/download/v${VERSION}/cmake-${VERSION}-Linux-x86_64.tar.gz -O cmake.tar.gz
-tar -xzf cmake.tar.gz
-
-# Remove the original files and copy the new ones over.
-sudo rm -rf /usr/local/cmake-3.12.4/*
-sudo cp -r cmake-${VERSION}*/* /usr/local/cmake-3.12.4/
-which cmake
-cmake --version
+    cmake --version
